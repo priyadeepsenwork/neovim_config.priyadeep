8e9bdf37082d153ed05358ae6307f9a043aaa42c
priyadeepsenwork <work.priyadeepsen@gmail.com>
2025-08-05 14:29:57 +0530

    trauma

--- init.lua
+++ init.lua
@@ -44,9 +44,11 @@ require("lazy").setup({
     { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",       enabled = true },
 
     -- LSP + Autocomplete (optional)
-    --   { "neovim/nvim-lspconfig", enabled = true },
-    --   { "hrsh7th/nvim-cmp", enabled = true },
-    --   { "hrsh7th/cmp-nvim-lsp", enabled = true },
+    { "neovim/nvim-lspconfig",           enabled = true },
+    { "hrsh7th/nvim-cmp",                enabled = true },
+    { "hrsh7th/cmp-nvim-lsp",            enabled = true },
+    { "L3MON4D3/LuaSnip",                enabled = true }, -- required for cmp
+    { "saadparwaiz1/cmp_luasnip",        enabled = true }, -- bridge between cmp and LuaSnip
 })
 
 -- TREE-SITTER CONFIGURATION
@@ -101,6 +103,9 @@ vim.opt.undofile = true           -- Persistent undo
 vim.opt.updatetime = 300          -- Faster completion
 vim.opt.signcolumn = 'yes'        -- Always show sign column
 
+vim.opt.laststatus = 2
+vim.opt.showmode = true -- show mode in status line 
+
 -- SYNTAX HIGHLIGHTING
 vim.cmd('syntax enable')
 
@@ -113,25 +118,25 @@ require('catppuccin').setup({
     transparent_background = true,
     term_colors = true,
     styles = {
-        comments = { 'italic' },
-        conditionals = { 'italic' },
-        loops = { 'italic' },
-        functions = { 'bold' },
-        keywords = { 'bold' },
-        strings = { 'italic' },
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
+        -- comments = { 'italic' },
+        -- conditionals = { 'italic' },
+        -- loops = { 'italic' },
+        -- functions = { 'bold' },
+        -- keywords = { 'bold' },
+        -- strings = { 'italic' },
+        variables = {'bold'},
+        -- numbers = {},
+        -- booleans = {},
+        -- properties = {},
+        -- types = {},
     },
 })
 
 vim.cmd.colorscheme('catppuccin') -- Set the colorscheme
 
 -- STATUS LINE (MINIMAL)
-vim.opt.laststatus = 2
-vim.opt.showmode = false
+-- vim.opt.laststatus = 2
+-- vim.opt.showmode = false
 
 -- SEARCH SETTINGS
 vim.opt.ignorecase = true -- Ignore case in search
@@ -146,3 +151,110 @@ vim.opt.splitbelow = true
 -- ENCODING
 vim.opt.fileencoding = 'utf-8'
 vim.scriptencoding = 'utf-8'
+
+-- ========================
+-- KEYMAPS
+-- ========================
+
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- File operations
+map("n", "<leader>w", ":w<CR>", opts)    -- Save file
+map("n", "<leader>q", ":q<CR>", opts)    -- Quit
+map("n", "<leader>n", ":enew<CR>", opts) -- New file
+
+-- Split navigation
+map("n", "<C-h>", "<C-w>h", opts)
+map("n", "<C-j>", "<C-w>j", opts)
+map("n", "<C-k>", "<C-w>k", opts)
+map("n", "<C-l>", "<C-w>l", opts)
+
+-- Split resizing
+map("n", "<A-Left>", ":vertical resize -2<CR>", opts)
+map("n", "<A-Right>", ":vertical resize +2<CR>", opts)
+map("n", "<A-Up>", ":resize -2<CR>", opts)
+map("n", "<A-Down>", ":resize +2<CR>", opts)
+
+-- NvimTree toggle
+map("n", "<leader>e", ":NvimTreeToggle<CR>", opts)
+
+-- Clear search highlights
+map("n", "<leader>h", ":nohlsearch<CR>", opts)
+
+-- Space as leader key
+vim.g.mapleader = " "
+
+
+-- ========================
+-- LSP + Autocompletion
+-- ========================
+
+-- CMP SETUP
+local cmp = require("cmp")
+local luasnip = require("luasnip")
+
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        ['<C-Space>'] = cmp.mapping.complete(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+        ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+    }),
+    sources = {
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+    },
+})
+
+-- LSP CONFIG
+local lspconfig = require("lspconfig")
+
+-- List of languages mentioned in treesitter setup
+local servers = {
+    "clangd",     -- for c, cpp
+    "rust_analyzer",
+    "pyright",
+    "jsonls",
+    "yamlls",
+    "bashls",
+    "marksman",   -- for markdown
+}
+
+-- Setup each server
+for _, server in ipairs(servers) do
+    lspconfig[server].setup({
+        capabilities = require('cmp_nvim_lsp').default_capabilities(),
+    })
+end
+
+
+-- Disable unwanted LSP features
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+
+
+
+
